588e78437ef5f029493c5282bf20a0f888fd8e8a
nehagode23 <118003687+nehagode23@users.noreply.github.com>
2025-02-04 16:25:16 +0530

    added api slices

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -2,7 +2,6 @@ import React,{useState,useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { Row,Col, Image, ListGroup,Card, Button } from 'react-bootstrap'
-import products from '../products'
 import Rating from '../components/Rating'
 import axios from 'axios'
 
