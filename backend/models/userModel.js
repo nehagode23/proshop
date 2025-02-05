2b652a03f11e8691092fc46de533b1fd4cbb773f
nehagode23 <118003687+nehagode23@users.noreply.github.com>
2025-02-05 15:14:43 +0530

    userid password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { type } from "os";
+import bcrypt from 'bcryptjs';
 
 const userSchema= new mongoose.Schema({
     name:{
@@ -23,6 +24,10 @@ const userSchema= new mongoose.Schema({
 },{
     timestamps:true,
 });
+
+userSchema.methods.matchPassword= async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 
 const User= mongoose.model('User', userSchema);
 export default User;
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ const port= 5000;
 
 connectDB(); //connect to mongodb
 const app=express();
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req, res)=>{
     res.send('API is running...');
