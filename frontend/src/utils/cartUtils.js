eb44bcfa2d099a590a3a25a4f9bc239b721d621f
nehagode23 <118003687+nehagode23@users.noreply.github.com>
2025-02-05 14:36:59 +0530

    section 6 done

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -2,7 +2,18 @@ export const addDecimals=(num)=>{
     return (Math.round(num*100)/100).toFixed(2);
 }
 
-export const updateCart=(state)=>{
+export const updateCart=(state,item)=>{
+    const existItem = state.cartItems.find((x) => x._id === item._id);
+  if (existItem) {
+    // If exists, update quantity
+    state.cartItems = state.cartItems.map((x) =>
+      x._id === existItem._id ? item : x
+    );
+  } else {
+    // If not exists, add new item to cartItems
+    state.cartItems = [...state.cartItems, item];
+  }
+  
     //calcuate item price
     state.itemsPrice=addDecimals( state.cartItems.reduce((acc,item)=>acc+item.price*item.qty, 0));
     //tax price(15%)
