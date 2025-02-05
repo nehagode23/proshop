d5d2fdf767a1413118f46bd26a0a622b78e08dfe
nehagode23 <118003687+nehagode23@users.noreply.github.com>
2025-02-05 12:45:12 +0530

    add toc art

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,16 @@
-import {Navbar,Nav,Container} from 'react-bootstrap';
+import {Navbar,Nav,Container, Badge} from 'react-bootstrap';
 import {FaShoppingCart,FaUser} from 'react-icons/fa'
 import {LinkContainer} from 'react-router-bootstrap'
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+
+    const{cartItems}= useSelector((state)=>state.cart);
+
   return (
     <header>
         <Navbar bg="dark" variant='dark' expand='md' collapseOnSelect> 
@@ -20,7 +24,11 @@ const Header = () => {
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
                         <Link to='/cart'>
-                        <Nav.Link href='/cart'><FaShoppingCart/>Cart</Nav.Link>
+                        <Nav.Link href='/cart'><FaShoppingCart/>Cart {cartItems.length>0 &&(
+                            <Badge pill bg='success' style={{marginLeft:'5px'}}>
+                                {cartItems.reduce((a,c)=>a+c.qty,0)}
+                            </Badge>
+                        )}</Nav.Link>
                         </Link>
                     </Nav>
                     <Nav className='ms-auto'>
---
+++ frontend/src/utils/cartUtils.js
@@ -0,0 +1,17 @@
+export const addDecimals=(num)=>{
+    return (Math.round(num*100)/100).toFixed(2);
+}
+
+export const updateCart=(state)=>{
+    //calcuate item price
+    state.itemsPrice=addDecimals( state.cartItems.reduce((acc,item)=>acc+item.price*item.qty, 0));
+    //tax price(15%)
+    state.taxPrice=addDecimals(Number((0.15*state.itemsPrice).toFixed(2)));
+    //shipping price (if order> 100$ = free)
+    state.shippingPrice= addDecimals(state.itemsPrice>100? 0:10); 
+    //total price
+    state.totalPrice=(Number(state.itemsPrice)+ Number(state.shippingPrice)+Number(state.taxPrice)).toFixed(2);
+
+    localStorage.setItem('cart',JSON.stringify(state));
+    return state;
+}
