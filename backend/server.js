88ffc64c35117a6607172399890a8f3f170ebc80
nehagode23 <118003687+nehagode23@users.noreply.github.com>
2025-02-17 11:16:55 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,6 @@ app.use(express.urlencoded({extended:true}));
 //cookie parser middleware
 app.use(cookieParser());
 
-app.get('/',(req, res)=>{
-    res.send('API is running...');
-});
-
 app.use('/api/products',productRoutes);
 app.use('/api/user',userRoutes);
 app.use('/api/orders',orderRoutes);
