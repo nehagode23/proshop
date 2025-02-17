0fffd59d1b4d3590a512d73f470e8afb2328177c
nehagode23 <118003687+nehagode23@users.noreply.github.com>
2025-02-17 09:38:29 +0530

    build

--- backend/server.js
+++ backend/server.js
@@ -35,4 +35,13 @@ app.get('/api/config/paypal',(req,res)=>res.send({clientId: process.env.PAYPAL_C
 const __dirname=path.resolve();
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')));
 
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'/frontend/build')));
+
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'frontend','build','index.html')));
+}else{
+    app.get('/',(req,res)=>{
+        res.send('API is running...');
+    });
+}
 app.listen(port,()=>console.log(`Server running on port ${port}`));
